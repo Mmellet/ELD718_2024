7a01c880232dd1adff4e90bbddc9e0f95cb9a663
Margot Mellet <40469215+Mmellet@users.noreply.github.com>
2024-10-09 11:22:38 -0400

    Update deploy.sh test update delpoy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
+
+
+# Optimisation des images JPEG avec une compression agressive (réduction de qualité à 85%)
+jpegoptim --max=85 *.jpg
+
+
+# Étape 1: Exécuter Hugo pour générer le contenu dans le répertoire temp
 hugo --cleanDestinationDir --environment epub -d temp
+
+# Étape 2: Naviguer dans le répertoire généré par Hugo
 cd temp
+
+find uploads -name '*.jpg' -exec jpegoptim {} \;
+
+# Étape 4: Supprimer l'ancien fichier EPUB s'il existe
 rm -f ../ebook.epub
-zip -rX ../ebook.epub mimetype content META-INF uploads
+
+# Étape 5: Créer l'EPUB avec le fichier mimetype non compressé
+zip -X0 ../ebook.epub mimetype
+
+# Étape 6: Ajouter le reste des fichiers avec compression
+zip -rX ../ebook.epub content META-INF uploads
+
